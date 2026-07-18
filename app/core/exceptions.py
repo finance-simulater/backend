b5f50f6776a7ce96cce6080e61391edd2e7225be
JeonSuna <jsa020910@gmail.com>
2026-07-18 18:01:09 +0900

    feat:예외상태 추가

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -3,3 +3,11 @@ from fastapi import HTTPException, status
 
 def not_found(detail: str) -> HTTPException:
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+def conflict(detail: str) -> HTTPException:
+    return HTTPException(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+def unprocessable(detail: str) -> HTTPException:
+    return HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
